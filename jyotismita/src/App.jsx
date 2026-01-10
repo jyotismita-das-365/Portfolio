fdcab15442ad0e808c2792694bc301ad033311bc
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-01-10 10:06:57 +0530

    start developing my personal portfolio, start with react js

---
+++ jyotismita/src/App.jsx
@@ -0,0 +1,17 @@
+import About from './components/About'
+// import Contact from './components/Contact'
+// import Projects from './components/Projects'
+// import Skills from './components/Skills'
+
+function App() {
+  return (
+    <>
+      <About />
+      {/* <Contact />
+      <Projects />
+      <Skills /> */}
+    </>
+  )
+}
+
+export default App
---
+++ jyotismita/src/components/About.jsx
@@ -0,0 +1,9 @@
+const About =() =>{
+  return (
+    <>
+    <h1 className="bg-red-700">jhghjghjgbn</h1>
+    </>
+  );
+};
+
+export default About;
